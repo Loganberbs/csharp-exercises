84c6e297881f60a04b4432846573e40ae953f810
Loganberbs <58014408+Loganberbs@users.noreply.github.com>
2020-09-29 13:44:24 -0500

    2.5.3

--- Program.cs
+++ Program.cs
@@ -4,21 +4,20 @@ namespace data_types
 {
     class Program
     {
-            public static void Main(string[] args)
-            {
+                public static void Main(string[] args)
+                {
             Console.WriteLine("What is your name?");
 
                 string name = Console.ReadLine();
 
             Console.WriteLine("Hello " + name);
 
-
                 double area;
                 double length;
                 double width;
                 string input;
 
-                Console.WriteLine("How longs your 'rectangle'? :");
+                Console.WriteLine("How long's your 'tangle'? :");
                 input = Console.ReadLine();
                 length = double.Parse(input);
             
@@ -29,8 +28,28 @@ namespace data_types
                 area = length * width;
                 
                 Console.WriteLine("The area of your tangle is: " + area);
-                Console.ReadLine();
-            }
+
+
+            Console.WriteLine("How many miles have you driven? ");
+                        
+                        double gallons;
+                        double miles;
+                        double mpg;
+                
+                input = Console.ReadLine();
+                miles = double.Parse(input);
+
+            Console.WriteLine("How many gallons of gas was used?");
+
+                input = Console.ReadLine();
+                gallons = double.Parse(input);
+                
+                 mpg = miles / gallons;
+
+            Console.WriteLine("The fuel efficency of your vehicle is: "+mpg+"mpg, or Miles Per Gallon.");
+
+
+        }
         }
     }
 
